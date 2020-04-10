905decf3d541d0dad4ac7fe7879574cd7322ec9d
Stanlox <maxim.bondaruk2000@mail.ru>
2020-04-10 19:54:16 +0300

    Add FileCabinetService.cs file.

--- FileCabinetApp/FileCabinetRecord.cs
+++ FileCabinetApp/FileCabinetRecord.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace FileCabinetApp
 {
-    class FileCabinetRecord
+    public class FileCabinetRecord
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
     }
 }
